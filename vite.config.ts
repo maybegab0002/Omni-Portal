ddeb2fae287b567b2b2abd164ab0c1c3a3e43612
maybegab0002 <gabenciso119@gmail.com>
2025-02-21 14:18:04 +0800

    Add GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,10 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: './postcss.config.cjs',
-  },
+  base: '/Omni-Portal/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
